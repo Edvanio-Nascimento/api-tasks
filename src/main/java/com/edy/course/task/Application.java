2d955d4ac284244d0984377da4572536031e212b
Edy Nascimento <nascoliedy@gmail.com>
2025-07-15 15:38:17 -0400

    renamed arquivo

--- src/main/java/com/edy/course/task/Application.java
+++ src/main/java/com/edy/course/task/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiTaskApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiTaskApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
